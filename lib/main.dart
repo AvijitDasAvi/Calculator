11a57b61824f34138ff9eb106d4608a0977778c5
avijit-das <avijitavi338895@gmail.com>
2023-12-03 11:43:01 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:calculator/screens/calculator_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData.dark(),
+      home: const CalculatorScreen(),
+    );
+  }
+}
