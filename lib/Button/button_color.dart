72a879bf022a39bd57c4f13a9dca4b73508fa4e1
avijit-das <avijitavi338895@gmail.com>
2023-12-12 15:55:29 +0600

    Updated

---
+++ lib/Button/button_color.dart
@@ -0,0 +1,22 @@
+import 'package:calculator/values/button_values.dart';
+import 'package:flutter/material.dart';
+
+//Its use for different color of different button
+Color buttonColor(value) {
+  return [Button.clr].contains(value)
+      ? Colors.red
+      : [Button.del].contains(value)
+          ? Colors.redAccent
+          : [
+              Button.per,
+              Button.multiply,
+              Button.add,
+              Button.divide,
+              Button.subtract,
+              Button.calculate
+            ].contains(value)
+              ? Colors.orange
+              : [Button.dot].contains(value)
+                  ? Colors.white24
+                  : Colors.black;
+}
--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -1,3 +1,5 @@
+import 'package:calculator/Button/build_button.dart';
+import 'package:calculator/values/button_values.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorScreen extends StatefulWidget {
@@ -10,26 +12,43 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenSize =
+        MediaQuery.of(context).size; //using for every device screen size
     return Scaffold(
       body: SafeArea(
           bottom: false,
           child: Column(
             children: [
               //Output section
-              SingleChildScrollView(
-                child: Container(
-                  alignment: Alignment.bottomRight,
-                  padding: const EdgeInsets.all(16),
-                  child: const Text(
-                    '0',
-                    style: TextStyle(
-                      fontSize: 48.0,
-                      fontWeight: FontWeight.bold,
+              Expanded(
+                child: SingleChildScrollView(
+                  reverse: true, //using for top to bottom
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.all(16),
+                    child: const Text(
+                      '0',
+                      style: TextStyle(
+                        fontSize: 48.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.end,
                     ),
-                    textAlign: TextAlign.end,
                   ),
                 ),
               ),
+              Wrap(
+                children: Button.buttonValues
+                    .map(
+                      (value) => SizedBox(
+                        //same height and width for every devices
+                        height: screenSize.width / 5,
+                        width: screenSize.width / 4,
+                        child: buildButton(value),
+                      ),
+                    )
+                    .toList(),
+              )
             ],
           )),
     );
